7355e08450926ec71d533c45a4da8ba86b4b1160
Bunyak Andriy <19091997x@gmail.com>
2019-04-07 19:21:50 +0000

    js

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function addToBasket () {
+  alert('Ваше замовлення додане до корзини');
+};
+
+
+const linkFacebook = document.querySelector('.link-facebook');
+const linkInstagram = document.querySelector('.link-instagram');
+const linkTelegram = document.querySelector('.link-telegram');
+
+linkFacebook.href = 'https://www.facebook.com';
+linkInstagram.href = 'https://www.instagram.com';
+linkTelegram.href = 'https://web.telegram.org';
