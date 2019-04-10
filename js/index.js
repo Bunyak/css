401f51e07af813596cadc01e3f7ad9fea05fbf0e
Bunyak Andriy <19091997x@gmail.com>
2019-04-10 21:41:16 +0000

    correction form and smth map

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@ function addToBasket () {
   alert('Ваше замовлення додане до корзини');
 };
 
-
 const linkFacebook = document.querySelector('.link-facebook');
 const linkInstagram = document.querySelector('.link-instagram');
 const linkTelegram = document.querySelector('.link-telegram');
@@ -11,4 +10,3 @@ linkFacebook.href = 'https://www.facebook.com';
 linkInstagram.href = 'https://www.instagram.com';
 linkTelegram.href = 'https://web.telegram.org';
 
-// Перше зробив через addEventListeners, потім хотів зробити для всіх кнопок і запутався, і вже не зміг зробити ніяку кнопку через addEventListeners 
